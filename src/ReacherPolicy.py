5c160ee47af0b6ef5ec640ba9082cd4e13ff97c7
Daniel Sotingco <daniel.sotingco@gmail.com>
2020-07-12 12:19:30 -0400

    bugfix: Make the following fixes to account for forward() being special in PyTorch: 1. Input/output should be tensors. 2. Do everything in forward() to encapsulate all forward-pass logic. 3. Don't call forward() directly; use model(x) to perform forward pass and output predictions.

--- src/ReacherPolicy.py
+++ src/ReacherPolicy.py
@@ -21,40 +21,29 @@ class ReacherPolicy(nn.Module):
         # Output of neural network: [mu1; mu2; mu3; mu4]
         self.std_deviations = nn.Parameter(init_std_deviation * torch.ones(1, self.action_size))
 
-    def calculate_distribution_means(self, state):
-        """ Calculate mean values to be used, using the neural network. """
+    def forward(self, state, actions=None, use_sampling=True):
+        """ Run the neural network and sample the distribution for actions. """
+        assert(torch.isnan(state).any() == False)
+        self.train()
+
         x = F.relu(self.fc1(state))
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
         means = torch.tanh(self.fc4(x))
-        return means
-
-    def forward(self, state, use_sampling=True):
-        """ Run the neural network and sample the distribution for actions. """
-        state = torch.tensor(state, dtype=torch.float)
-        self.train()
-        means = self.calculate_distribution_means(state)
         m = torch.distributions.normal.Normal(means, self.std_deviations)
-        if use_sampling:
-            raw_nn_actions = m.sample()
-        else:
-            raw_nn_actions = means
-        # Since distribution sampling may yield values beyond [-1, 1],
-        # saturate the action values.
-        actions = torch.clamp(raw_nn_actions, -1.0, 1.0)
-        log_probs = m.log_prob(actions)
-        return (actions, log_probs)
 
-    def calculate_log_probs_from_actions(self, state, actions):
-        """ Calculate log probabilities from state and actions.  
-            To be used for calculating new probabilities as the 
-            policy changes during training. """
-        # NOTE: These are technically not log probabilities, but rather
-        # logs of the probability density functions. 
-        assert(torch.isnan(state).any() == False)
+        if actions is None:
+            if use_sampling:
+                raw_nn_actions = m.sample()
+            else:
+                raw_nn_actions = means
+            # Since distribution sampling may yield values beyond [-1, 1],
+            # saturate the action values.
+            actions = torch.clamp(raw_nn_actions, -1.0, 1.0)
         assert(torch.isnan(actions).any() == False)
-        self.train()
-        means = self.calculate_distribution_means(state)
-        m = torch.distributions.normal.Normal(means, self.std_deviations)
+
+        # NOTE: These are technically not log probabilities, but rather
+        # logs of the probability density functions.
         log_probs = m.log_prob(actions)
-        return log_probs
+        
+        return (actions, log_probs)
